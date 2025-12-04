11c8821f915a06ee928c957286634273921e8531
Flo Kleiser <floriankleiser@gmail.com>
2025-12-04 14:49:21 +0100

    warn log start

--- js/main.js
+++ js/main.js
@@ -2,12 +2,24 @@ let emergencyToggle = false
 let emergencyButton = document.getElementById('emergency-btn');
 let navbar = document.getElementById('navbar');
 
+let warnlogToggle = false
+let warnlogButton = document.getElementById('log-btn');
+let warnlogPanel = document.getElementById('log-panel');
 
-if (emergencyButton && navbar) {
-  emergencyButton.addEventListener('click', function() {
+
+// if (emergencyButton && navbar) {
+emergencyButton.addEventListener('click', function() {
     emergencyToggle = !emergencyToggle;
     emergencyButton.innerHTML = emergencyToggle ? 'Resolve Emergency' : 'Emergency';
     emergencyButton.classList.toggle('active', emergencyToggle);
     navbar.classList.toggle('emergency', emergencyToggle);
-  });
+});
+// }
+
+if (warnlogButton && warnlogPanel) {
+    warnlogButton.addEventListener('click', function() {
+    warnlogToggle = !warnlogToggle;
+    // warnlogButton.classList.toggle('active', warnlogToggle);
+    warnlogPanel.style.display = warnlogToggle ? 'block' : 'none';
+});
 }
